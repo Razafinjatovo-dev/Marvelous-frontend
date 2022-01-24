04acd6f8d47ef5bc2fc748976f0d4c12b9fe2d66
Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>
2022-01-24 20:27:54 +0100

    fix charact details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 // import Cookies from "js-cookie";
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, {useState} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Characters from "./Containers/Characters/Characters";
 import Comics from "./Containers/Comics/Comics";
 import Header from "./Containers/Headers/Header";
@@ -9,72 +9,74 @@ import CharacterDetails from "./Components/CharacterDetails/CharacterDetails";
 import FavoriteCharacters from "./Components/FavoriteCharacters/FavoriteCharacters";
 import FavoriteComics from "./Components/FavoriteComics/FavoriteComics";
 import Favorites from "./Containers/Favorites/Favorites";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHeart, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faHeart, faSearch} from "@fortawesome/free-solid-svg-icons";
 import LoaderSpinner from "./Components/Loader/LoaderSpinner";
+
 library.add(faHeart, faSearch);
 
 function App() {
-  //Characters Favorites
-  const Url = "https://marvelous-backend.herokuapp.com";
-  const [
-    favoriteCharactersPopulated,
-    setFavoriteCharactersPopulated,
-  ] = useState([]);
-  const [favoriteCharactersList, setFavoriteCharactersList] = useState([]);
-  //Comics Favorites
-  const [favoriteComicsIdList, setFavoriteComicsIdList] = useState([]);
-  const [favoriteComicsPopulated, setFavoriteComicsPopulated] = useState([]);
+    //Characters Favorites
+    const Url = "https://marvelous-backend.herokuapp.com";
+    // const Url = "https://lereacteur-marvel-api.herokuapp.com/";
+    const [
+        favoriteCharactersPopulated,
+        setFavoriteCharactersPopulated,
+    ] = useState([]);
+    const [favoriteCharactersList, setFavoriteCharactersList] = useState([]);
+    //Comics Favorites
+    const [favoriteComicsIdList, setFavoriteComicsIdList] = useState([]);
+    const [favoriteComicsPopulated, setFavoriteComicsPopulated] = useState([]);
 
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/Comics">
-          <Comics
-            Url={Url}
-            favoriteComicsIdList={favoriteComicsIdList}
-            setFavoriteComicsIdList={setFavoriteComicsIdList}
-            favoriteComicsPopulated={favoriteComicsPopulated}
-            setFavoriteComicsPopulated={setFavoriteComicsPopulated}
-          />
-        </Route>
-        <Route path="/FavoriteComics">
-          <FavoriteComics
-            favoriteComicsIdList={favoriteComicsIdList}
-            favoriteComicsPopulated={favoriteComicsPopulated}
-          />
-        </Route>
-        <Route path="/FavoriteCharacters">
-          <FavoriteCharacters
-            favoriteCharactersList={favoriteCharactersList}
-            setFavoriteCharactersList={setFavoriteCharactersList}
-            favoriteCharactersPopulated={favoriteCharactersPopulated}
-            setFavoriteCharactersPopulated={setFavoriteCharactersPopulated}
-          />
-        </Route>
-        <Route exact path="/CharacterDetails/:characterId">
-          <CharacterDetails Url={Url} />
-        </Route>
-        <Route exact path="/Favorites">
-          <Favorites />
-        </Route>
-        <Route exact path="/">
-          <Characters
-            Url={Url}
-            favoriteCharactersList={favoriteCharactersList}
-            setFavoriteCharactersList={setFavoriteCharactersList}
-            favoriteCharactersPopulated={favoriteCharactersPopulated}
-            setFavoriteCharactersPopulated={setFavoriteCharactersPopulated}
-          />
-        </Route>
-        <Route path="/Loader">
-          <LoaderSpinner/>
-        </Route>
-      </Switch>
-    </Router>
+    return (
+        <Router>
+            <Header/>
+            <Switch>
+                <Route path="/Comics">
+                    <Comics
+                        Url={Url}
+                        favoriteComicsIdList={favoriteComicsIdList}
+                        setFavoriteComicsIdList={setFavoriteComicsIdList}
+                        favoriteComicsPopulated={favoriteComicsPopulated}
+                        setFavoriteComicsPopulated={setFavoriteComicsPopulated}
+                    />
+                </Route>
+                <Route path="/FavoriteComics">
+                    <FavoriteComics
+                        favoriteComicsIdList={favoriteComicsIdList}
+                        favoriteComicsPopulated={favoriteComicsPopulated}
+                    />
+                </Route>
+                <Route path="/FavoriteCharacters">
+                    <FavoriteCharacters
+                        favoriteCharactersList={favoriteCharactersList}
+                        setFavoriteCharactersList={setFavoriteCharactersList}
+                        favoriteCharactersPopulated={favoriteCharactersPopulated}
+                        setFavoriteCharactersPopulated={setFavoriteCharactersPopulated}
+                    />
+                </Route>
+                <Route exact path="/CharacterDetails/:characterId">
+                    <CharacterDetails Url={Url}/>
+                </Route>
+                <Route exact path="/Favorites">
+                    <Favorites/>
+                </Route>
+                <Route exact path="/">
+                    <Characters
+                        Url={Url}
+                        favoriteCharactersList={favoriteCharactersList}
+                        setFavoriteCharactersList={setFavoriteCharactersList}
+                        favoriteCharactersPopulated={favoriteCharactersPopulated}
+                        setFavoriteCharactersPopulated={setFavoriteCharactersPopulated}
+                    />
+                </Route>
+                <Route path="/Loader">
+                    <LoaderSpinner/>
+                </Route>
+            </Switch>
+        </Router>
 
-  );
+    );
 }
 
 export default App;
--- src/Components/CharacterDetails/CharacterDetails.js
+++ src/Components/CharacterDetails/CharacterDetails.js
@@ -1,61 +1,64 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
+import LoaderSpinner from '../Loader/LoaderSpinner';
 import "./CharacterDetails.css";
 
 const CharacterDetails = (props) => {
-  const { characterId, Url} = useParams();
-  const [comicsList, setcomicsList] = useState();
-  const [characterName, setCharacterName] = useState();
-  const [characterPictureUrl, setCharacterPictureUrl] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+    const {Url} = props
+    const {characterId} = useParams();
+    const [comicsList, setcomicsList] = useState();
+    const [characterName, setCharacterName] = useState();
+    const [characterPictureUrl, setCharacterPictureUrl] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
-  // console.log("Id personnage " + characterId);
-  //fetch data
-  useEffect(() => {
-    const fetchComicsList = async () => {
-      const response = await axios.get(
-        `${Url}/comics/${characterId}`
-      );
-      //   console.log(response.data.comics);
-      setCharacterName(response.data.name);
-      setCharacterPictureUrl(
-        response.data.thumbnail.path + "." + response.data.thumbnail.extension
-      );
-      setcomicsList(response.data.comics);
-      setIsLoading(false);
-    };
-    fetchComicsList();
-  }, [characterId]);
+    // console.log("Id personnage " + characterId);
+    //fetch data
+    useEffect(() => {
+        const fetchComicsList = async () => {
+            const response = await axios.get(
+                `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=40D67RDGCM1a6Bfj`
+            );
+            //   console.log(response.data.comics);
+            console.log(response);
+            setCharacterName(response.data.name);
+            setCharacterPictureUrl(
+                response.data?.thumbnail.path + "." + response.data.thumbnail.extension
+            );
+            setcomicsList(response.data.comics);
+            setIsLoading(false);
+        };
+        fetchComicsList();
+    }, [characterId]);
 
-  return isLoading === true ? (
-    <p>Loading page...</p>
-  ) : (
-    <div className="MainContent_GlobalWrapper">
-      <div className="CharacterDetails_pictureWrapper">
-        <img src={characterPictureUrl} alt="characterPhoto" />
-      </div>
-      <p className="CharacterName">{characterName}</p>
-      <h3>COMICS LIST</h3>
-      <div className='ComicsList_Wrapper'>
-        {comicsList.map((comic, index) => {
-          return (
-            <div className="Comic_Card" key={index}>
-              <div className='ComicPicture_Wrapper'>
-                <img src={comic.thumbnail.path +
-                        "." +
-                        comic.thumbnail.extension} alt="ComicPicture" />
-              </div>
-              <p className="ComicTitle">{comic.title}</p>
-              <p className="ComicDescription">{comic.description && `Description ${comic.description}`}</p>
-              {/* <p>Description: {comic.description}</p> */}
-              <hr></hr>
+    return isLoading === true ? (
+        <LoaderSpinner/>
+    ) : (
+        <div className="MainContent_GlobalWrapper">
+            <div className="CharacterDetails_pictureWrapper">
+                <img src={characterPictureUrl} alt="characterPhoto"/>
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+            <p className="CharacterName">{characterName}</p>
+            <h3>COMICS LIST</h3>
+            <div className='ComicsList_Wrapper'>
+                {comicsList.map((comic, index) => {
+                    return (
+                        <div className="Comic_Card" key={index}>
+                            <div className='ComicPicture_Wrapper'>
+                                <img src={comic.thumbnail.path +
+                                "." +
+                                comic.thumbnail.extension} alt="ComicPicture"/>
+                            </div>
+                            <p className="ComicTitle">{comic.title}</p>
+                            <p className="ComicDescription">{comic.description && `Description ${comic.description}`}</p>
+                            {/* <p>Description: {comic.description}</p> */}
+                            <hr></hr>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default CharacterDetails;
